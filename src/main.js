a0f2e90594984365f011f4798ae1277fd1565a18
zhaodakeng <a9611576@qq.com>
2022-08-24 11:29:03 +0800

    date原型增加了获取昨天的日期getYestoday()函数 Number原型增加了判断是否为浮点型isFloat函数

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
  * @returns {undefined|*}
  */
 Array.prototype.last = function () {
-    if (this.length == 0)
+    if (this.length === 0)
         return undefined
     return this[this.length - 1]
 }
@@ -23,13 +23,13 @@ Date.prototype.format = function (fmt) {
     }
     for (var k in o) {
         if (new RegExp("(" + k + ")").test(fmt)) {
-            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
         }
     }
     return fmt;
 }
 
-String.prototype.copyStr = function (){
+String.prototype.copyStr = function () {
     let aux = document.createElement("input");
     aux.setAttribute("value", this);
     document.body.appendChild(aux);
@@ -38,8 +38,27 @@ String.prototype.copyStr = function (){
     document.body.removeChild(aux);
 }
 
+
+/**
+ * 获取昨天的日期
+ * @returns {*}
+ */
+Date.prototype.getYestoday = function () {
+    return new Date(Date.now() - 1000 * 60 * 60 * 24).format("yyyy-MM-dd")
+}
+
+/**
+ * 判断是否浮点数，使用：Number().isFloat(number,3)
+ * @param number
+ * @returns {boolean}
+ */
+Number.prototype.isFloat = function (number) {
+    let val = number || this
+    return !isNaN(Number(val)) && val.toString().indexOf('.') > -1
+}
+
 module.exports = {
-    test(){
+    test() {
         console.log('test')
     }
 }
